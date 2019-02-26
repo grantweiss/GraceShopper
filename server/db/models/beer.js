5d824ab6d772a7657c2f4911b2bb5335f4d31bbb
Kyle Lee <Kylecrlee95@gmail.com>
2019-02-26 10:11:29 -0600

    compelted sequelize model for single beer

---
+++ server/db/models/beer.js
@@ -0,0 +1,42 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Beer = db.define('beer', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: true
+  },
+  price: {type: Sequelize.FLOAT},
+  inventory: {type: Sequelize.INTEGER, defaultValue: 0, allowNull: false},
+  abv: {type: Sequelize.FLOAT},
+  ibu: {type: Sequelize.INTEGER},
+  type: {
+    type: Sequelize.ENUM(
+      'Pilsner',
+      'Lager',
+      'Bock',
+      'Stout',
+      'IPA',
+      'Wheat',
+      'Red Ale',
+      'Amber Ale',
+      'Hefeweizen',
+      'Lambic'
+    )
+  },
+  imgURL: {
+    type: Sequelize.STRING,
+    defaultValue:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2000px-No_image_available.svg.png',
+    validate: {
+      isUrl: true
+    }
+  }
+})
