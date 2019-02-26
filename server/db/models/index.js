3ff15be235da5c35d9c72f444f63bc7c6ddcf24b
Grant <grant.sweiss@gmail.com>
2019-02-26 13:29:52 -0600

    Models associations (#15)
    * add model associations
    
    * fix model export for beer, and finalize associations
    
    * add user / orderItem association

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,3 +1,4 @@
+const db = require('../db')
 const User = require('./user')
 const Brewery = require('./brewery')
 const Order = require('./order')
@@ -5,7 +6,7 @@ const Image = require('./image')
 const Category = require('./category')
 const Beer = require('./beer')
 const Review = require('./review')
-
+const OrderItem = require('./orderItem')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -19,12 +20,38 @@ const Review = require('./review')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+Beer.belongsTo(Brewery)
+Brewery.hasMany(Beer)
+
+User.hasMany(Order)
+Order.belongsTo(User)
+
+OrderItem.belongsTo(User)
+User.hasMany(OrderItem)
+
+OrderItem.belongsTo(Order)
+Order.hasMany(OrderItem)
+
+OrderItem.belongsTo(Beer)
+Beer.hasMany(OrderItem)
+
+Review.belongsTo(User)
+User.hasMany(Review)
+Review.belongsTo(Beer)
+Beer.hasMany(Review)
+
+Category.belongsToMany(Beer, {through: 'categoryBeer'})
+Beer.belongsToMany(Category, {through: 'categoryBeer'})
+
 module.exports = {
+  db,
   User,
   Brewery,
   Order,
   Image,
   Category,
   Beer,
-  Review
+  Review,
+  OrderItem
 }
