b9fc024337c534f5b2948fafd37a205c99b97178
Steve Bonifas <45438367+sbonifas@users.noreply.github.com>
2019-02-27 16:30:18 -0600

    Completed User model tests and Beer model tests (#34)
    * Completed review model
    
    * Completed User and Beer Model tests

--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -8,7 +8,57 @@ describe('User model', () => {
   beforeEach(() => {
     return db.sync({force: true})
   })
+  describe('Instance fields', () => {
+    let cody
+    beforeEach(async () => {
+      cody = await User.create({
+        firstName: 'Cody',
+        lastName: 'Fullstack',
+        phoneNumber: '773-773-7373',
+        userType: 'auth',
+        email: 'cody@puppybook.com',
+        password: 'bones'
+      })
+    })
 
+    describe('firstName field', () => {
+      it('has a firstName field where type is string', () => {
+        expect(cody.firstName).to.be.a('string')
+        expect(cody.firstName).to.equal('Cody')
+      })
+    })
+    describe('lastName field', () => {
+      it('has a lastName field where type is string', () => {
+        expect(cody.lastName).to.be.a('string')
+        expect(cody.lastName).to.equal('Fullstack')
+      })
+    })
+    describe('phoneNumber field', () => {
+      it('has a phoneNumber field where type is string', () => {
+        expect(cody.phoneNumber).to.be.a('string')
+        expect(cody.phoneNumber).to.equal('773-773-7373')
+      })
+    })
+    describe('userType field', () => {
+      it('has a userType field where type is string', () => {
+        expect(cody.userType).to.be.a('string')
+        expect(cody.userType).to.equal('auth')
+      })
+      it('requires `userType` to be one of an enumerator list', async () => {
+        let result, error
+        try {
+          result = await cody.update({userType: 'Bad User'})
+        } catch (err) {
+          error = err
+        }
+        if (result)
+          throw Error(
+            'validation should fail when type is not in the enumerator list'
+          )
+        expect(error).to.be.an.instanceOf(Error)
+      })
+    })
+  })
   describe('instanceMethods', () => {
     describe('correctPassword', () => {
       let cody
