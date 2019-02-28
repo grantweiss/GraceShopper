268f0af4a361f38951894091c936dde5001be63a
Steve Bonifas <stevenbonifas@gmail.com>
2019-02-28 14:48:36 -0600

    Fixed another bug in Google Login

---
+++ secretsHeroku.js
@@ -0,0 +1,5 @@
+process.env.GOOGLE_CLIENT_ID =
+  '164772240650-2bg0m9kojc6f57ulhcg0nillj9gmo23k.apps.googleusercontent.com'
+process.env.GOOGLE_CLIENT_SECRET = 'ysaG_4j7zmiy2x0IH3s9aXt4'
+process.env.GOOGLE_CALLBACK =
+  'https://front-right-of-room-pod.herokuapp.com/auth/google/callback'
