0ef82785b843060aba63882a2e52cc1751f6b6f3
Grant Weiss <grant.sweiss@gmail.com>
2019-03-01 14:49:44 -0600

    travis imgURL bug bites back

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -14,35 +14,45 @@ export const UserHome = props => {
       <h3>Welcome, {email}</h3>
       <h3>Featured Beer of the Month</h3>
       <br />
-      <Container>
-        <Row>
-          <Col md={{span: 6, offset: 3}}>
-            <Card>
-              <Row>
-                <Col xs={4}>
-                  <Card.Img
-                    className="thumbNail small-pad-bottom"
-                    src={beer.imgURL}
-                  />
-                  <Link to="/beers/1">
-                    <Button>See Beer</Button>
-                  </Link>
-                </Col>
-                <Col xs={8}>
-                  <Card.Body className="align-left">
-                    <Card.Title>
-                      {beer.title}{' '}
-                      <span className="small-txt">{beer.type}</span>
-                    </Card.Title>
-                    <Card.Text>{beer.description}</Card.Text>
-                    <Card.Text>${beer.price}</Card.Text>
-                  </Card.Body>
-                </Col>
-              </Row>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+      <div className="center">
+        {beer ? (
+          <Container>
+            <Row>
+              <Col md={{span: 6, offset: 3}}>
+                <Card>
+                  <Row>
+                    <Col xs={12} sm={4}>
+                      <Card.Img
+                        className="thumbNail small-pad-bottom"
+                        src={beer.imgURL}
+                      />
+                    </Col>
+                    <Col xs={12} sm={8}>
+                      <Card.Body className="align-left">
+                        <Card.Title>
+                          {beer.title}{' '}
+                          <span className="small-txt">{beer.type}</span>
+                        </Card.Title>
+                        <Card.Text>{beer.description}</Card.Text>
+                        <Card.Text>
+                          ${beer.price}
+                          <span className="float-right">
+                            <Link to="/beers/1">
+                              <Button>See Beer</Button>
+                            </Link>
+                          </span>
+                        </Card.Text>
+                      </Card.Body>
+                    </Col>
+                  </Row>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        ) : (
+          <h1>No beer</h1>
+        )}
+      </div>
     </div>
   )
 }
