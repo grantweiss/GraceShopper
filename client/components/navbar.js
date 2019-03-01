a09a87320cc0d53fca938932c147359a402a7091
Alex <alexrgura@gmail.com>
2019-03-01 14:58:31 -0600

    basic cart implemented

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -15,6 +15,7 @@ const Navbar = ({handleClick, isLoggedIn, isAdmin}) => (
             <Link to="/home">Home</Link>
             <Link to="/beers/page/1">See Beers</Link>
             <Link to="/addbeer">Add a Beer</Link>
+            <Link to="/cart">See Cart</Link>
             <a href="#" onClick={handleClick}>
               Logout
             </a>
@@ -24,6 +25,7 @@ const Navbar = ({handleClick, isLoggedIn, isAdmin}) => (
             {/* The navbar will show these links after you log in */}
             <Link to="/home">Home</Link>
             <Link to="/beers/page/1">See Beers</Link>
+            <Link to="/cart">See Cart</Link>
             <a href="#" onClick={handleClick}>
               Logout
             </a>
@@ -35,6 +37,7 @@ const Navbar = ({handleClick, isLoggedIn, isAdmin}) => (
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
           <Link to="/beers/page/1">See Beers</Link>
+          <Link to="/cart">See Cart</Link>
         </div>
       )}
     </nav>
