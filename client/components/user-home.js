2a4337b8dd912954be4cabc0a18af878bf2394e8
Grant Weiss <grant.sweiss@gmail.com>
2019-03-01 14:37:42 -0600

    travis

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -8,46 +8,41 @@ import {Link} from 'react-router-dom'
  * COMPONENT
  */
 export const UserHome = props => {
-  console.log(props)
   const {email, beer} = props
   return (
     <div className="center">
       <h3>Welcome, {email}</h3>
       <h3>Featured Beer of the Month</h3>
       <br />
-      {beer ? (
-        <Container>
-          <Row>
-            <Col md={{span: 6, offset: 3}}>
-              <Card>
-                <Row>
-                  <Col xs={4}>
-                    <Card.Img
-                      className="thumbNail small-pad-bottom"
-                      src={beer.imgURL}
-                    />
-                    <Link to="/beers/1">
-                      <Button>See Beer</Button>
-                    </Link>
-                  </Col>
-                  <Col xs={8}>
-                    <Card.Body className="align-left">
-                      <Card.Title>
-                        {beer.title}{' '}
-                        <span className="small-txt">{beer.type}</span>
-                      </Card.Title>
-                      <Card.Text>{beer.description}</Card.Text>
-                      <Card.Text>${beer.price}</Card.Text>
-                    </Card.Body>
-                  </Col>
-                </Row>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
-      ) : (
-        <h1>No beer</h1>
-      )}
+      <Container>
+        <Row>
+          <Col md={{span: 6, offset: 3}}>
+            <Card>
+              <Row>
+                <Col xs={4}>
+                  <Card.Img
+                    className="thumbNail small-pad-bottom"
+                    src={beer.imgURL}
+                  />
+                  <Link to="/beers/1">
+                    <Button>See Beer</Button>
+                  </Link>
+                </Col>
+                <Col xs={8}>
+                  <Card.Body className="align-left">
+                    <Card.Title>
+                      {beer.title}{' '}
+                      <span className="small-txt">{beer.type}</span>
+                    </Card.Title>
+                    <Card.Text>{beer.description}</Card.Text>
+                    <Card.Text>${beer.price}</Card.Text>
+                  </Card.Body>
+                </Col>
+              </Row>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
