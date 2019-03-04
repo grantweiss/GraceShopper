ee7a6cc98a6708c141b81264e070e3b3bf069256
Steve Bonifas <stevenbonifas@gmail.com>
2019-03-04 09:54:11 -0600

    Fixed Bug

--- server/api/orders.js
+++ server/api/orders.js
@@ -13,25 +13,6 @@ const Sequelize = require('sequelize')
 const Op = Sequelize.Op
 module.exports = router
 
-const isLoggedIn = (req, res, next) => {
-  if (req.user) next()
-  else {
-    const err = new Error('Must loggin to do things')
-    res.status(401)
-    next(err)
-  }
-}
-
-const isAdmin = (req, res, next) => {
-  if (req.user.userType === 'admin') {
-    next()
-  } else {
-    const err = new Error('Must be an admin')
-    res.status(401)
-    next(err)
-  }
-}
-
 //CREATE ORDER
 router.post('/', async (req, res, next) => {
   try {
