ddb6f8411667d2d44d54baff9f2aa89fa14f99c9
Alex <alexrgura@gmail.com>
2019-03-05 18:22:51 -0600

    my profile nav

--- client/routes.js
+++ client/routes.js
@@ -90,7 +90,7 @@ class Routes extends Component {
           </Switch>
         )},
         {/* Displays our Login component as a fallback */}
-        <Route component={Login} />ç
+        <Route component={Login} />
       </Switch>
     )
   }
